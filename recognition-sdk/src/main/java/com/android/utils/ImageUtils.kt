ef91ac6170f0c5ac35c9915f4e460189d4517981
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2021-03-19 23:51:21 +0330

    add image utils to get new name, get gallery path and ...

---
+++ recognition-sdk/src/main/java/com/android/utils/ImageUtils.kt
@@ -0,0 +1,24 @@
+package com.android.utils
+
+import android.os.Environment
+
+internal object ImageUtils {
+
+    private const val IMAGE_NAME_PREFIX = "text_recognition_"
+    private const val IMAGE_NAME_SUFFIX = ".jpg"
+
+
+    fun getGalleryPath(): String {
+        return Environment.getExternalStorageDirectory().toString() +
+                "/" +
+                Environment.DIRECTORY_PICTURES + "/".also { it }
+    }
+
+    fun getNewFileName(): String {
+        return StringBuilder().apply {
+            append(IMAGE_NAME_PREFIX)
+            append(System.currentTimeMillis())
+            append(IMAGE_NAME_SUFFIX)
+        }.toString()
+    }
+}
