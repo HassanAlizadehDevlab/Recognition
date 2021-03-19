b8b7a4b931ab1cdc207adc2e8d062bac6d54390f
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2021-03-19 23:54:28 +0330

    add TextRecognition class for host app to get access to sdk

---
+++ recognition-sdk/src/main/java/com/android/recognition_sdk/text/TextRecognition.kt
@@ -0,0 +1,45 @@
+package com.android.recognition_sdk.text
+
+import android.content.Intent
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * TextRecognition is a class for encapsulating text recognition usages between SDK and Host app.
+ * */
+object TextRecognition {
+
+    private const val TEXT_RECOGNITION_DATA = "data"
+
+
+    /** Generate success intent for internal usage */
+    internal fun getSuccessResultIntent(data: String): Intent {
+        return Intent().apply {
+            with(TextRecognitionResult.Success(data)) {
+                putExtra(TEXT_RECOGNITION_DATA, this)
+            }
+        }
+    }
+
+    /** Generate failure intent for internal usage */
+    internal fun getErrorResultIntent(error: String?): Intent {
+        return Intent().apply {
+            with(TextRecognitionResult.Error(error)) {
+                putExtra(TEXT_RECOGNITION_DATA, this)
+            }
+        }
+    }
+
+}
+
+
+/**
+ * Model class for response of text recognition
+ * */
+sealed class TextRecognitionResult : Parcelable {
+    @Parcelize
+    data class Success(val data: String) : TextRecognitionResult()
+
+    @Parcelize
+    data class Error(val message: String?) : TextRecognitionResult()
+}
